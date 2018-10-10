1eb0313cfd4ee752c28d4a46b8d58d5c14e47e82
Oswald Ramirez <oswaldr@live.com>
2018-10-10 16:45:06 -0400

    10102018: Updated split of functionality
    Continued updates to split the functionality into separate objects that can be independently run.

--- VW_INT_Agg_MonthlyDonorsPerLocation.py
+++ VW_INT_Agg_MonthlyDonorsPerLocation.py
@@ -10,24 +10,7 @@ from config import db
 from models import (
     VW_INT_Agg_MonthlyDonorsPerLocation,
     VW_INT_Agg_MonthlyDonorsPerLocationSchema,
-    VW_INT_Agg_DailyDonorsPerLocation,
-    VW_INT_Agg_DailyDonorsPerLocationSchema,
 )
-
-
-def read_all_daily():
-    """
-    This function responds to a request for /api/people
-    with the complete lists of people
-    :return:        json string of list of people
-    """
-    # Create the list of people from our data
-    dailyDonors = VW_INT_Agg_DailyDonorsPerLocation.query.order_by(VW_INT_Agg_DailyDonorsPerLocation.yearmonthdayNum).all()
-
-    # Serialize the data for the response
-    dailyDonors_schema = VW_INT_Agg_DailyDonorsPerLocationSchema(many=True)
-    data = dailyDonors_schema.dump(VW_INT_Agg_DailyDonorsPerLocation).data
-    return data
 
     
 def read_all_monthly():
@@ -41,5 +24,5 @@ def read_all_monthly():
 
     # Serialize the data for the response
     monthlyDonors_schema = VW_INT_Agg_MonthlyDonorsPerLocationSchema(many=True)
-    data = monthlyDonors_schema.dump(VW_INT_Agg_MonthlyDonorsPerLocation).data
+    data = monthlyDonors_schema.dump(monthlyDonors).data
     return data
